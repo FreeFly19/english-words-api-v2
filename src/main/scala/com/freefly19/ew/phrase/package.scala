8e02b28da90e59efadd8bf148b94ec1ccfe93eb5
FreeFly19 <oleksandr.melnyk19@gmail.com>
2018-08-03 01:35:32 +0300

    Add createAt field to phrase. Implement Timestamp encoder

--- src/main/scala/com/freefly19/ew/phrase/package.scala
+++ src/main/scala/com/freefly19/ew/phrase/package.scala
@@ -1,9 +1,12 @@
 package com.freefly19.ew
 
+import java.sql.Timestamp
+
 import cats.effect.IO
 import doobie.free.connection.ConnectionIO
 import doobie.implicits._
 import doobie.util.transactor.Transactor
+import io.circe.Encoder
 import io.circe.generic.auto._
 import io.circe.syntax._
 import org.http4s._
@@ -12,11 +15,13 @@ import org.http4s.dsl.io._
 
 
 package object phrase {
-  case class Phrase(id: Long, text: String)
+  implicit val timestampEncoder: Encoder[Timestamp] = a => a.getTime.asJson
+
+  case class Phrase(id: Long, text: String, createdAt: Timestamp)
 
   class PhraseRepository {
     def findAll: ConnectionIO[List[Phrase]] =
-      sql"select id, text from phrases"
+      sql"select id, text, created_at as createdAt from phrases"
         .query[Phrase]
         .to[List]
   }
